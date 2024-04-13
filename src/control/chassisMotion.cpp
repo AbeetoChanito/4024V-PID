10c982742834be19e5cc097f5b438b0227abbed3
AbeetoChanito <132693471+AbeetoChanito@users.noreply.github.com>
2024-04-13 12:42:45 -0400

    move chassis motion algs into seperate file

---
+++ src/control/chassisMotion.cpp
@@ -0,0 +1,88 @@
+#include "control/chassis.hpp"
+
+#include <cmath>
+
+void Chassis::brakeMotors() {
+    m_leftMotor->set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
+    m_rightMotor->set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
+    m_leftMotor->brake();
+    m_rightMotor->brake();
+}
+
+void Chassis::setHeading(float heading) {
+    m_imu.set_rotation(heading);
+}
+
+static float shortenPath(float error) {
+    return std::remainder(error, 360);
+}
+
+static float capSpeed(float speed, float maxSpeed) {
+    return std::signbit(speed) * std::min(std::abs(speed), maxSpeed);
+}
+
+void Chassis::turn(float targetHeading, float maxSpeed) {
+    m_angularPID->reset();
+    m_angularSettled.reset();
+
+    while (!m_angularSettled->isSettled()) {
+        float error = shortenPath(targetHeading - m_imu.get_rotation());
+
+        float output = capSpeed(m_angularPID->update(error), maxSpeed);
+
+        arcadeControl(0, output);
+    }
+    
+    brakeMotors();
+}
+
+void Chassis::swing(float targetHeading, SwingType swingType, float maxSpeed) {
+    m_swingPID->reset();
+    m_swingSettled.reset();
+
+    if (swingType == SwingType::LeftSwing) {
+        m_rightMotor->set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
+        m_rightMotor->brake();
+    } else {
+        m_leftMotor->set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
+        m_leftMotor->brake();
+    }
+
+    while (!m_angularSettled->isSettled()) {
+        float error = shortenPath(targetHeading - m_imu.get_rotation());
+
+        float output = capSpeed(m_swingPID->update(error), maxSpeed);
+
+        if (swingType == SwingType::LeftSwing) {
+            tankControl(output, 0);
+        } else {
+            tankControl(0, output);
+        }
+    }
+    
+    if (swingType == SwingType::LeftSwing) {
+        m_leftMotor->set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
+        m_leftMotor->brake();
+    } else {
+        m_rightMotor->set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
+        m_rightMotor->brake();
+    }
+}
+
+void Chassis::move(float targetDistance, float maxSpeed) {
+    m_lateralPID->reset();
+    m_lateralSettled.reset();
+
+    m_leftMotor->tare_position_all();
+    m_rightMotor->tare_position_all();
+
+    while (!m_lateralSettled->isSettled()) {
+        float error = (m_leftMotor->get_position(0) + m_rightMotor->get_position(0)) / 2 * m_gearRatio * m_wheelDiameter * M_PI;
+
+        float output = capSpeed(m_lateralPID->update(error), maxSpeed);
+        
+        arcadeControl(output, 0);
+    }
+
+    brakeMotors();
+}
