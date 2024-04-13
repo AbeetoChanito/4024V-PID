ac26447da72566fe1fdd494b81a2b68c8715511a
AbeetoChanito <132693471+AbeetoChanito@users.noreply.github.com>
2024-04-13 09:05:35 -0400

    Finish opcontrol

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@
 
 pros::Controller controller(pros::E_CONTROLLER_MASTER);
 
+// chassis config
 auto leftMotors = std::make_shared<pros::MotorGroup>(std::initializer_list<std::int8_t>{1, -2, 3}, pros::v5::MotorGears::blue);
 auto rightMotors = std::make_shared<pros::MotorGroup>(std::initializer_list<std::int8_t>{-8, 9, -10}, pros::v5::MotorGears::blue);
 pros::IMU imu(18);
@@ -32,6 +33,18 @@ Chassis chassis(
 	swingPID,
 	swingSettled
 );
+
+// intake
+pros::MotorGroup intake(std::initializer_list<int8_t> {11, 20});
+
+// wings
+pros::adi::Pneumatics wingFrontLeft('b', false);
+pros::adi::Pneumatics wingFrontRight('c', false);
+pros::adi::Pneumatics wingBackLeft('d', false);
+
+// hang
+pros::adi::Pneumatics hangUp('e', false);
+pros::adi::Pneumatics hangDown('f', false);
 
 /**
  * Runs initialization code. This occurs as soon as the program is started.
@@ -88,7 +101,77 @@ void autonomous() {}
  * task, not resume it from where it left off.
  */
 void opcontrol() {
+	constexpr pros::controller_digital_e_t INTAKE_BUTTON = pros::E_CONTROLLER_DIGITAL_L1;
+	constexpr pros::controller_digital_e_t OUTTAKE_BUTTON = pros::E_CONTROLLER_DIGITAL_L2;
+
+	constexpr pros::controller_digital_e_t FRONT_LEFT_WING = pros::E_CONTROLLER_DIGITAL_RIGHT;
+	constexpr pros::controller_digital_e_t FRONT_RIGHT_WING = pros::E_CONTROLLER_DIGITAL_Y;
+	constexpr pros::controller_digital_e_t BACK_LEFT_WING = pros::E_CONTROLLER_DIGITAL_R2;
+
+	constexpr pros::controller_digital_e_t HANG_BUTTON = pros::E_CONTROLLER_DIGITAL_R1;
+
+	constexpr pros::controller_digital_e_t REVERSE_BUTTON = pros::E_CONTROLLER_DIGITAL_X;
+
+	pros::Mutex hangMutex;
+
+	bool hangedUp = false;
+
 	while (true) {
-		pros::delay(20); // Run for 20 ms then update
+		if (controller.get_digital(INTAKE_BUTTON)) {
+			intake.move(127);
+		} else if (controller.get_digital(OUTTAKE_BUTTON)) {
+			intake.move(-127);
+		} else {
+			intake.move(0);
+		}
+
+		if (controller.get_digital(FRONT_LEFT_WING)) {
+			wingFrontLeft.extend();
+		} else {
+			wingFrontLeft.retract();
+		}
+
+		if (controller.get_digital(FRONT_RIGHT_WING)) {
+			wingFrontRight.extend();
+		} else {
+			wingFrontRight.retract();
+		}
+
+		if (controller.get_digital(BACK_LEFT_WING)) {
+			wingBackLeft.extend();
+		} else {
+			wingBackLeft.retract();
+		}
+
+		if (controller.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_UP)) {
+			if (hangedUp) {
+				pros::Task hangTask([&](){
+					hangMutex.take();
+					hangUp.extend();
+					pros::delay(250);
+					hangUp.retract();
+					hangMutex.give();
+				});
+			} else {
+				pros::Task hangTask([&](){
+					hangMutex.take();
+					hangDown.extend();
+					hangMutex.give();
+				});
+			}
+
+			hangedUp = true;
+		}
+
+		float left = controller.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
+		float right = controller.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
+
+		if (controller.get_digital(REVERSE_BUTTON)) {
+			chassis.tankControl(-left, -right);
+		} else {
+			chassis.tankControl(left, right);
+		}
+
+ 		pros::delay(10);
 	}
 }
