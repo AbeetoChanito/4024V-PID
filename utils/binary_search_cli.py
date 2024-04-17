51e6d20bde40f45c10b97bb70152d4ca7e649e9a
AbeetoChanito <132693471+AbeetoChanito@users.noreply.github.com>
2024-04-17 09:45:53 -0400

    format documents

--- utils/binary_search_cli.py
+++ utils/binary_search_cli.py
@@ -1,60 +1,64 @@
 class InputState:
-  HIGH = 0
-  LOW = 1
-  STOP = 2
+    HIGH = 0
+    LOW = 1
+    STOP = 2
 
-  @staticmethod
-  def get_input_state():
-    text = input("input state: ")
+    @staticmethod
+    def get_input_state():
+        text = input("input state: ")
 
-    if text == "high":
-      return InputState.HIGH
-    if text == "low":
-      return InputState.LOW
-    if text == "stop":
-      return InputState.STOP
-      
-    print("invalid input, try again")
-    return InputState.get_input_state()
+        if text == "high":
+            return InputState.HIGH
+        if text == "low":
+            return InputState.LOW
+        if text == "stop":
+            return InputState.STOP
+
+        print("invalid input, try again")
+        return InputState.get_input_state()
+
 
 def state_2X(k, k_name):
-  while True:
-    k *= 2
-    print(f"(stage 2X) try {k_name}: {k:.2f}")
+    while True:
+        k *= 2
+        print(f"(stage 2X) try {k_name}: {k:.2f}")
 
-    state = InputState.get_input_state()
-    
-    if state == InputState.HIGH:
-      k /= 2
-      return k
+        state = InputState.get_input_state()
 
-    if state == InputState.STOP:
-      return k
+        if state == InputState.HIGH:
+            k /= 2
+            return k
+
+        if state == InputState.STOP:
+            return k
+
 
 def state_binary_search(k, k_name):
-  last_high = k * 2
-  last_low = 0
+    last_high = k * 2
+    last_low = 0
 
-  while True:
-    print(f"(stage binary search) try {k_name}: {k:.2f}")
+    while True:
+        print(f"(stage binary search) try {k_name}: {k:.2f}")
 
-    state = InputState.get_input_state()
+        state = InputState.get_input_state()
 
-    if state == InputState.HIGH:
-      last_k = k
-      k = (k + last_low) / 2
-      last_high = last_k
-    elif state == InputState.LOW:
-      last_k = k
-      k = (last_high + k) / 2
-      last_low = last_k
-    elif state == InputState.STOP:
-      return k
+        if state == InputState.HIGH:
+            last_k = k
+            k = (k + last_low) / 2
+            last_high = last_k
+        elif state == InputState.LOW:
+            last_k = k
+            k = (last_high + k) / 2
+            last_low = last_k
+        elif state == InputState.STOP:
+            return k
+
 
 def state_tune_constant(k, k_name):
-  k = state_2X(k, k_name)
-  k = state_binary_search(k, k_name)
-  return k
+    k = state_2X(k, k_name)
+    k = state_binary_search(k, k_name)
+    return k
+
 
 kp = float(input("(stage begin) input kp: "))
 kd = float(input("(stage begin) input kd: "))
@@ -62,7 +66,7 @@ kd = float(input("(stage begin) input kd: "))
 i = 0
 
 while True:
-  kp = state_tune_constant(kp, "kp")
-  kd = state_tune_constant(kd, "kd")
-  print(f"(stage {i + 1}) kp: {kp:.2f}, kd: {kd:.2f}")
-  i += 1
+    kp = state_tune_constant(kp, "kp")
+    kd = state_tune_constant(kd, "kd")
+    print(f"(stage {i + 1}) kp: {kp:.2f}, kd: {kd:.2f}")
+    i += 1
--- utils/grapher.py
+++ utils/grapher.py
@@ -1,6 +1,8 @@
 import subprocess
 import matplotlib.pyplot as plt
 
+# todo: don't get output directly from pros t
+
 COMMAND = ["pros", "t"]
 
 popen = subprocess.Popen(COMMAND, stdout=subprocess.PIPE, universal_newlines=True)
@@ -16,12 +18,12 @@ while True:
     if begin_reading is True:
         split_data = stdout_line.split(" ")
         plt.scatter(float(split_data[0]), float(split_data[1]), color="blue")
-    
+
     if stdout_line == "START":
         begin_reading = True
 
     print(stdout_line)
-    plt.pause(50 / 1000) 
+    plt.pause(50 / 1000)
 
 plt.show()
 
