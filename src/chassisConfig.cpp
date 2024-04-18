17f2a0e2a05209e84bc83d8c4610f6a8a9c85a03
AbeetoChanito <132693471+AbeetoChanito@users.noreply.github.com>
2024-04-18 15:34:17 -0400

    remove extraneous comments

--- src/chassisConfig.cpp
+++ src/chassisConfig.cpp
@@ -1,21 +1,12 @@
 #include "chassisConfig.hpp"
-
-// 9.6 0.32
-// 14.40 1.28
-// 19.8 1.92
-// 24.75 2.64
-// 30.55 3.30
 std::shared_ptr<PID> lateralPID = std::make_shared<PID>(30.55, 0, 3.30);
 std::shared_ptr<PIDSettled> lateralSettled = std::make_shared<PIDSettled>(
-    lateralPID, std::initializer_list<Bound>{{1, 0.5}, {0.5, 0}}, 0);
+    lateralPID, std::initializer_list<Bound>{{0.5, 0}}, 0);
 
 std::shared_ptr<PID> angularPID = std::make_shared<PID>(5, 0, 0.395);
 std::shared_ptr<PIDSettled> angularSettled = std::make_shared<PIDSettled>(
     angularPID, std::initializer_list<Bound>{{3, 0.25}}, 0);
 
-// 2.40 0.24
-// 4.80 0.42
-// 7.20 0.63
 std::shared_ptr<PID> swingPID = std::make_shared<PID>(10.80, 0, 1);
 std::shared_ptr<PIDSettled> swingSettled = std::make_shared<PIDSettled>(
     swingPID, std::initializer_list<Bound>{{3, 0.25}}, 0);
